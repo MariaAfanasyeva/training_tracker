3946156cf9ad6e35e46684ed0b7896bd1ef8c75a
Maryia Afanasyeva <Maryia.Afanasyeva@itechart-group.com>
2022-08-11 21:41:07 +0300

    added pre-commit hooks

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_ENGINE")
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['DEBUG'] = os.getenv("DEBUG")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["DEBUG"] = os.getenv("DEBUG")
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return 'Hello, world'
+    return "Hello, world"
--- models.py
+++ models.py
@@ -5,40 +5,40 @@ class Distance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     distance = db.Column(db.Numeric(10, 2))
     units = db.Column(db.String(100))
-    sets = db.relationship('Set', backref='distance', lazy=True)
+    sets = db.relationship("Set", backref="distance", lazy=True)
 
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    exercises = db.relationship('Exercise', backref='group', lazy=True)
+    exercises = db.relationship("Exercise", backref="group", lazy=True)
 
 
 class Training(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     training_date = db.Column(db.Date)
     status = db.Column(db.String(255))
-    sets = db.relationship('Set', backref='training', lazy=True)
+    sets = db.relationship("Set", backref="training", lazy=True)
 
 
 class Weight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     weight = db.Column(db.Numeric(10, 2))
     units = db.Column(db.String(100))
-    sets = db.relationship('Set', backref='weight', lazy=True)
+    sets = db.relationship("Set", backref="weight", lazy=True)
 
 
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=True)
-    sets = db.relationship('Set', backref='exercise', lazy=True)
+    group_id = db.Column(db.Integer, db.ForeignKey("group.id"), nullable=True)
+    sets = db.relationship("Set", backref="exercise", lazy=True)
 
 
 class Set(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     exercise_count = db.Column(db.Integer)
-    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'), nullable=False)
-    training_id = db.Column(db.Integer, db.ForeignKey('training.id'), nullable=False)
-    distance_id = db.Column(db.Integer, db.ForeignKey('distance.id'), nullable=True)
-    weight_id = db.Column(db.Integer, db.ForeignKey('weight.id'), nullable=True)
+    exercise_id = db.Column(db.Integer, db.ForeignKey("exercise.id"), nullable=False)
+    training_id = db.Column(db.Integer, db.ForeignKey("training.id"), nullable=False)
+    distance_id = db.Column(db.Integer, db.ForeignKey("distance.id"), nullable=True)
+    weight_id = db.Column(db.Integer, db.ForeignKey("weight.id"), nullable=True)
