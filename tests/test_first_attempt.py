70e6fc1914a950e2b35b7461bf8de912f6b4e93a
Maryia Afanasyeva <masha.afanaseva0192@gmail.com>
2023-06-23 22:39:31 +0300

    test status code for get_all

--- tests/test_first_attempt.py
+++ tests/test_first_attempt.py
@@ -1,4 +1,30 @@
 import pytest
 
-def test_first():
-    assert 2 + 2 == 4
+from app import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.from_pyfile("config_test.py")
+
+    # other setup can go here
+
+    yield app
+    #TODO: add here drop database
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+@pytest.mark.parametrize("endpoint", [("/groups"), ("/users"), ("/weights"), ("/distances"), ("/exercises"), ("/sets"), ("/trainings")])
+def test_status_code_get_all_groups(client, endpoint):
+    response = client.get(endpoint)
+    assert response.status_code == 200
